12cc0276e978140b1f625b784ad20a2739cd1d84
sfelker <sfelker@cs.washington.edu>
2014-12-03 13:41:38 -0800

    added logic to adjust player values based on hits

--- src/main/java/com/unikitty/project3/Attack.java
+++ src/main/java/com/unikitty/project3/Attack.java
@@ -2,14 +2,16 @@ package com.unikitty.project3;
 
 public class Attack extends GameEntity {
 	private int ownerID;
-	
-	public Attack(int owner, int attackID, float x, float y, float xVel, float yVel) {
+	private int atkDmg;
+
+	public Attack(int owner, int attackID, float x, float y, float xVel, float yVel, int pwr) {
 		ownerID = owner;
 		id = attackID;
 		xPos = x;
 		yPos = y;
 		xVelocity = xVel;
 		yVelocity = yVel;
+		atkDmg = pwr;
 	}
 
 	public int getOwnerID() {
@@ -19,4 +21,12 @@ public class Attack extends GameEntity {
 	public void setOwnerID(int ownerID) {
 		this.ownerID = ownerID;
 	}	
+	
+	public int getAtkPwr() {
+		return atkDmg;
+	}
+
+	public void setAtkPwr(int atkDmg) {
+		this.atkDmg = atkDmg;
+	}
 }
