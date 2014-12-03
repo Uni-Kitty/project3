4225a7ecc8e81affabc14d24071049fb47bda829
John Wilson <johnleew@geert.cs.washington.edu>
2014-12-03 13:41:35 -0800

    progress

--- src/main/java/com/unikitty/project3/Attack.java
+++ src/main/java/com/unikitty/project3/Attack.java
@@ -1,7 +1,14 @@
 package com.unikitty.project3;
 
 public class Attack extends GameEntity {
+    
+    public static final String FIREBALL = "fireball";
+    public static final String ARROW = "arrow";
+    
 	private int ownerID;
+	private String type;
+	
+	public Attack() {}
 	
 	public Attack(int owner, int attackID, float x, float y, float xVel, float yVel) {
 		ownerID = owner;
@@ -18,5 +25,13 @@ public class Attack extends GameEntity {
 
 	public void setOwnerID(int ownerID) {
 		this.ownerID = ownerID;
-	}	
+	}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }	
 }
